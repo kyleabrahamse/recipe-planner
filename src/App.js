35c59cb101f8672480c4074da0718682f7c7d6e3
Kyle Abrahamse <kyleabrahamse@outlook.com>
2024-03-21 14:13:24 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import recipes from "./recipes";
+import { useState } from "react";
 
 function App() {
+  function getRandomObject(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+
+  let [randomRecipe, getRandomRecipe] = useState("");
+  let [numRecipes, setNumRecipes] = useState(0);
+
+  function random() {
+    getRandomRecipe((randomRecipe = getRandomObject(recipes)));
+  }
+
+  console.log(randomRecipe);
+
+  function handleChange(event) {
+    setNumRecipes(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Veggie Vibes</h1>
+      <h2>Meals</h2>
+      <div>
+        <input type="number" value={numRecipes} onChange={handleChange} />
+        <button onClick={random}>Generate Shopping List</button>
+      </div>
+      <h2>Shopping List</h2>
+      <p>{randomRecipe.name}</p>
     </div>
   );
 }
