ca3f54b1106ce71fa497be4ef35d01a25d46b918
Kyle Abrahamse <kyleabrahamse@outlook.com>
2024-03-23 18:32:33 +0000

    ingredients rendering on screen

--- src/App.js
+++ src/App.js
@@ -2,16 +2,49 @@ import recipes from "./recipes";
 import { useState } from "react";
 
 function App() {
+  const [weeklyRecipes, setWeeklyRecipes] = useState([]);
+  const [num, setNum] = useState(0);
+
+  function handleClick() {
+    const selectedRecipes = [];
+    for (let i = 0; i < num; i++) {
+      selectedRecipes.push(getRandomRecipe(recipes));
+    }
+    setWeeklyRecipes(selectedRecipes);
+  }
+
+  function handleChange(event) {
+    setNum(event.target.value);
+  }
+
+  function getRandomRecipe(arr) {
+    const random = Math.floor(Math.random() * arr.length);
+    return arr[random];
+  }
+
+  console.log(weeklyRecipes);
+
   return (
     <div className="App">
       <h1>Veggie Vibes</h1>
       <h2>Meals</h2>
       <ul></ul>
       <div>
-        <input />
-        <button>Generate Shopping List</button>
+        <input
+          type="number"
+          min="0"
+          max="7"
+          onChange={handleChange}
+          value={num}
+        />
+        <button onClick={handleClick}>Generate Shopping List</button>
       </div>
       <h2>Shopping List</h2>
+      <ul>
+        {weeklyRecipes.map((recipe, i) => (
+          <li key={i}>{recipe.ingredients}</li>
+        ))}
+      </ul>
     </div>
   );
 }
